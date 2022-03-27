817f1a3e711ad52b735e85f91587c1f947fd73f3
Arai-git <10l.mah.s11@gmail.com>
2022-03-28 07:29:35 +0900

    コードの修正とDBコード削除

--- src/public/user/signin.php
+++ src/public/user/signin.php
@@ -1,46 +1,17 @@
 <?php
 
 session_start();
+$registed = $_SESSION['registed'] ?? '';
+$error = $_SESSION['errors'] ?? '';
 
-$email = filter_input(INPUT_POST, 'email');
-$password = filter_input(INPUT_POST, 'password');
+$_SESSION['registed'] = '';
+unset($_SESSION['errors']);
 
-try {
-    $dbUserName = 'root';
-    $dbPassWord = 'password';
-    $pdo = new PDO(
-        'mysql:host=mysql;dbname=blog;charset=utf8',
-        $dbUserName,
-        $dbPassWord
-    );
-    $sql = 'SELECT * FROM users WHERE email = :email';
-    $statement = $pdo->prepare($sql);
-    $statement->bindValue(':email', $email, PDO::PARAM_STR);
-    $statement->execute();
-    $clientInfomation = $statement->fetch(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    $message = $e->getMessage();
-}
-
-if ($_POST['create']) {
-    if (password_verify($password, $clientInfomation['password'])) {
-        $_SESSION['id'] = $clientInfomation['id'];
-        $_SESSION['name'] = $clientInfomation['name'];
-        $_SESSION['email'] = $clientInfomation['email'];
-        $_SESSION['password'] = $clientInfomation['password'];
-        header('Location: ../index.php');
-    } else {
-        $message = 'メールアドレスもしくはパスワードが間違っています。';
-        echo $message;
-    }
-}
-
-if (empty($_SESSION['name'])) {
-    $name = 'ゲスト';
-} else {
-    $name = $_SESSION['name'];
+if(empty($name)) {
+    $name = "ゲスト";
 }
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -53,15 +24,14 @@ if (empty($_SESSION['name'])) {
       <h3>こんにちは、<?php echo $name; ?> さん</h3>
     </header>
     <h1>ログイン</h1>
-    <form action="" method="post">
-        <input type="text" name="email" size="20" placeholder="Email"></input>
-        <br>
-        <br>
-        <input type="password" name="password" size="20" placeholder="Password"></input>
-        <br>
-        <br>
-        <input type="submit" name="create" value="ログイン">
-        <br>
+    <form action="signin_complete.php" method="post">
+        <p><input type=“text” name="email" type="mail" required placeholder="Email" value="<?php if (
+            isset($_SESSION['email'])
+        ) {
+            echo $_SESSION['email'];
+        } ?>"></p>
+        <p><input type="password" placeholder="password" name="password" required></p>
+        <button type="submit">ログイン</button>
         <br>
         <a href="signup.php">アカウントを作る</a>
     </form>
