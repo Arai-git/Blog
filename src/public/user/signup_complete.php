47ab682c1895fa821fa0e99f3af82a928a6cbfdb
Arai-git <10l.mah.s11@gmail.com>
2022-04-25 15:45:59 +0900

    UseCase実装

--- src/public/user/signup_complete.php
+++ src/public/user/signup_complete.php
@@ -3,37 +3,37 @@ require_once __DIR__ . '/../../app/Infrastructure/Dao/UserDao.php';
 require_once __DIR__ . '/../../app/Infrastructure/Redirect/redirect.php';
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use App\Infrastructure\Dao\UserDao;
+use App\Usecase\UseCaseInput\SignUpInput;
+use App\Usecase\UseCaseInteractor\SignUpInteractor;
 
 $email = filter_input(INPUT_POST, 'email');
 $name = filter_input(INPUT_POST, 'name');
 $password = filter_input(INPUT_POST, 'password');
 $confirmPassword = filter_input(INPUT_POST, 'confirmPassword');
 
-session_start();
-if (empty($password) || empty($confirmPassword)) {
-    $_SESSION['errors'][] = 'パスワードを入力してください';
-}
-if ($password !== $confirmPassword) {
-    $_SESSION['errors'][] = 'パスワードが一致しません';
-}
-if (!empty($_SESSION['errors'])) {
+try {
+    session_start();
+    if (empty($password) || empty($confirmPassword)) {
+        throw new Exception('パスワードを入力してください');
+    }
+    if ($password !== $confirmPassword) {
+        throw new Exception('パスワードが一致しません');
+    }
+
+    //ガード節でここまで処理が行われれば、クラスの処理を行う。
+    $useCaseInput = new SignUpInput($name, $email, $password);
+    $useCase = new SignUpInteractor($useCaseInput);
+    $useCaseOutput = $useCase->handler();
+
+
+    if (!$useCaseOutput->isSuccess()) {
+        throw new Exception($useCaseOutput->message());
+    }
+    $_SESSION['message'][] = $useCaseOutput->message();
+    redirect('./signin.php');
+} catch (Exception $e) {
+    $_SESSION['errors'][] = $e->getMessage();
     $_SESSION['formInputs']['name'] = $name;
     $_SESSION['formInputs']['email'] = $email;
     redirect('./signup.php');
 }
-
-$userDao = new UserDao();
-$user = $userDao->findByEmail($email);
-
-if (!is_null($user)) {
-    $_SESSION['errors'][] = 'すでに登録済みのメールアドレスです';
-}
-if (!empty($_SESSION['errors'])) {
-    redirect('./signup.php');
-}
-
-$userDao->create($name, $email, $password);
-$_SESSION['message'] = '登録できました。';
-redirect('./signin.php');
-?>
