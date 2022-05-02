22ab4319f76ad1b1025a2f955d05401418bcbe8a
Arai-git <10l.mah.s11@gmail.com>
2022-05-02 14:52:49 +0900

    引数と返り値の指定とコメントの追記

--- src/app/Usecase/UseCaseOutput/SignUpOutput.php
+++ src/app/Usecase/UseCaseOutput/SignUpOutput.php
@@ -1,22 +1,45 @@
 <?php
-namespace App\Usecase\UseCaseOutput;
 
+namespace App\UseCase\UseCaseOutput;
+
+/**
+ * ユーザー登録ユースケースの出力値
+ */
 final class SignUpOutput
 {
+    /**
+     * @var bool
+     */
     private $isSuccess;
+
+    /**
+     * @var string
+     */
     private $message;
 
+    /**
+     * コンストラクタ
+     *
+     * @param boolean $isSuccess
+     * @param string $message
+     */
     public function __construct(bool $isSuccess, string $message)
     {
         $this->isSuccess = $isSuccess;
         $this->message = $message;
     }
 
+    /**
+     * @return boolean
+     */
     public function isSuccess(): bool
     {
         return $this->isSuccess;
     }
 
+    /**
+     * @return string
+     */
     public function message(): string
     {
         return $this->message;
